6956952c14543aa7dc4c9ecac1d37f9895194640
ZhekLu <justletmego@list.ru>
2021-06-02 23:08:49 +0300

    Add Barrirer class & Destroy fun

---
+++ TANKS/Barrier.cpp
@@ -0,0 +1,61 @@
+#include "Barrier.h"
+
+Barrier::Barrier(int ax, int ay, Type type, sf::Texture& texture)
+{
+    this->type = type; 
+
+    //init sprite
+    body.setTexture(texture);
+    SetRect();
+    body.setPosition(ax, ay); 
+}
+
+void Barrier::render(sf::RenderTarget* target)
+{
+    target->draw(body);
+}
+
+bool Barrier::loseHp(Rotation bulletRotation, int damage)
+{
+    if (type % 2)
+        return true; 
+    if(type <= 2)
+        Destroy(bulletRotation);
+    hp -= damage;
+    return hp > 0;
+}
+
+void Barrier::SetRect()
+{
+    switch (type)
+    {
+    case Barrier::IRON:
+    case Barrier::BRICK:
+        break;
+    case Barrier::IRON_HF_V:
+    case Barrier::BRICK_HF_V:
+        body.setTextureRect(sf::IntRect(0, 0, 20, 40));
+        break;
+    case Barrier::IRON_HF_H:
+    case Barrier::BRICK_HF_H:
+        body.setTextureRect(sf::IntRect(0, 0, 40, 20));
+        break;
+    }
+}
+
+void Barrier::Destroy(Rotation bulRot)
+{
+    switch (bulRot)
+    {
+    case Rotation::RIGHT:
+        body.move(20, 0); //40x40 picture; 
+    case Rotation::LEFT:
+        body.setTextureRect(sf::IntRect(0, 0, 20, 40));
+        break;
+    case Rotation::DOWN:
+        body.move(0, 20);
+    case Rotation::UP:
+        body.setTextureRect(sf::IntRect(0, 0, 40, 20));
+        break;
+    }
+}
---
+++ TANKS/Barrier.h
@@ -0,0 +1,31 @@
+#ifndef BARRIER_H
+#define BARRIER_H
+#include<SFML/Graphics.hpp>
+#include"definecells.h"
+
+class Barrier
+{
+public:
+    static enum Type : int
+    {
+        IRON = 1, BRICK, IRON_HF_V, BRICK_HF_V, IRON_HF_H, BRICK_HF_H
+    };
+
+    Barrier(int ax, int ay, Type type, sf::Texture& texture);
+    //functions
+    void render(sf::RenderTarget* target);
+    bool loseHp(Rotation bulletRotation, int damage = 1);
+private:
+    //    variables
+    int hp;
+    Type type;
+
+    //  elements
+    sf::Sprite body; 
+
+    //  fun 
+    void SetRect();
+    void Destroy(Rotation bulRot);
+};
+
+#endif // BARRIER_H
